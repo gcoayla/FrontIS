716a5e0de87c27279f2d20cb13ded941231d5bbf
JosnickCB <42551058+JosnickCB@users.noreply.github.com>
2020-08-10 00:00:34 -0500

    Creacion de adopciones

---
+++ BD/aux_for_adopciones.sql
@@ -0,0 +1,12 @@
+insert into enadopcion values(1,1,'Nombre: Firulais - Edad: 1 año - Macho','001','-','-','2020-08-05',1,1);
+insert into enadopcion values(2,1,'Nombre: Motta - Edad: 2 años - Macho','002','-','-','2020-07-21',1,1);
+insert into enadopcion values(3,1,'Nombre: Nick - Edad: 4 años - Macho','003','-','-','2020-06-14',1,1);
+insert into enadopcion values(4,1,'Nombre: Pelusa - Edad: 5 años - Macho','004','-','-','2020-01-16',1,1);
+insert into enadopcion values(5,1,'Nombre: Toby - Edad: 1 año - Macho','005','-','-','2020-02-15',1,1);
+insert into enadopcion values(6,1,'Nombre: Max - Edad: 2 años - Macho','006','-','-','2020-05-14',1,1);
+insert into enadopcion values(7,1,'Nombre: Hachiko - Edad: 4 años - Macho','007','-','-','2020-08-18',1,1);
+insert into enadopcion values(8,1,'Nombre: Goofy - Edad: 1 año - Macho','008','-','-','2020-07-15',1,1);
+insert into enadopcion values(9,1,'Nombre: Rex - Edad: 4 años - Macho','009','-','-','2020-05-11',1,1);
+insert into enadopcion values(10,1,'Nombre: Bruno - Edad: 2 años - Macho','010','-','-','2020-03-13',1,1);
+insert into enadopcion values(11,1,'Nombre: Draco - Edad: 2 años - Macho','011','-','-','2020-03-25',1,1);
+insert into enadopcion values(12,1,'Nombre: Enzo - Edad: 2 años - Macho','012','-','-','2020-02-15',1,1);
